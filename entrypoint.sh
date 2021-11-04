3cb6d8002b7718b1e830c22e2755bf3438ebd49f
oleksis <oleksis.fraga@gmail.com>
2021-11-04 06:08:42 -0400

    Use $HOME

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,10 @@
 # Fail on errors.
 set -e
 
+# Create if not exists
+touch $HOME/.bashrc
 # Make sure .bashrc is sourced
-. /root/.bashrc
+. $HOME/.bashrc
 
 WORKDIR=$GITHUB_WORKSPACE
 
@@ -17,14 +19,14 @@ if [[ "$PYPI_URL" != "https://pypi.python.org/" ]] || \
    [[ "$PYPI_INDEX_URL" != "https://pypi.python.org/simple" ]]; then
     # the funky looking regexp just extracts the hostname, excluding port
     # to be used as a trusted-host.
-    mkdir -p /root/.pip
-    echo "[global]" > /root/.pip/pip.conf
-    echo "index = $PYPI_URL" >> /root/.pip/pip.conf
-    echo "index-url = $PYPI_INDEX_URL" >> /root/.pip/pip.conf
-    echo "trusted-host = $(echo $PYPI_URL | perl -pe 's|^.*?://(.*?)(:.*?)?/.*$|$1|')" >> /root/.pip/pip.conf
+    mkdir -p $HOME/.pip
+    echo "[global]" > $HOME/.pip/pip.conf
+    echo "index = $PYPI_URL" >> $HOME/.pip/pip.conf
+    echo "index-url = $PYPI_INDEX_URL" >> $HOME/.pip/pip.conf
+    echo "trusted-host = $(echo $PYPI_URL | perl -pe 's|^.*?://(.*?)(:.*?)?/.*$|$1|')" >> $HOME/.pip/pip.conf
 
     echo "Using custom pip.conf: "
-    cat /root/.pip/pip.conf
+    cat $HOME/.pip/pip.conf
 fi
 
 cd $WORKDIR
@@ -41,7 +43,7 @@ if [ -f requirements.txt ]; then
 fi # [ -f requirements.txt ]
 
 # Source ~/.bashrc
-. /root/.bashrc
+. $HOME/.bashrc
 
 echo "PyInstaller parameters: $@"
 
